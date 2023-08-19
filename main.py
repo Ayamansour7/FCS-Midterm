0cbe183a487ef4a96b5542ca0d9e99694cebbadc
Ayamansour7 <141877589+Ayamansour7@users.noreply.github.com>
2023-08-20 00:38:27 +0300

    added

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 employees ={} #concerning an empty dictionary
 #https://stackoverflow.com/questions/61097144/reading-operations-line-by-line-and-making-eval-for-each-line
 #https://www.freecodecamp.org/news/python-import-from-file-importing-local-files-in-python/
@@ -14,7 +13,7 @@ def load_employees():
 
 def admin_menu(): #function for user menu
   while True:
-    print("/nAdmin Menu")
+    print("\nAdmin Menu")
     print("1 to Display Statistics")
     print("2 to Add an Employee")
     print("3 Display all Employees")
@@ -24,7 +23,7 @@ def admin_menu(): #function for user menu
     print("7 to Save and Exit")
   
 
-    choose=input("Enter your task number: ") #shoose whatever task they wanna make 
+    choose=input("Enter your task number: ") #choose whatever task they wanna make 
 
     if choose=="1":
       display_statistics()
@@ -44,12 +43,91 @@ def admin_menu(): #function for user menu
     else:
       print("Task not valid,Try again") #while entering any value different the ones mentioned
 
+
+
 #write the display statistics function to see how many males and females
 def display_statistics():
   males=0
   females=0
-  
+  for employee in employees.values():
+    #https://www.geeksforgeeks.org/python-dictionary-values/
+    if employee["gender"]=="male":
+      males=males+1
+    elif employee["gender"]=="female":
+      females=females+1
 
+  print("Number of male employees: ",males)
+  print("Number of female employees: ",females)
+  
+def add_employee(): 
+#to add new employee just add username,gender,and salary
+  username=input("Enter employee's username: ")
+  gender=input("Enter the gender: ")
+  salary=input("Add the salary: ")
+
+#id is incremented and the time is automatic
+
+  emp_id = "emp{}".format(len(employees) + 1)
+
+#https://www.tutorialspoint.com/how-to-add-timestamp-to-excel-file-in-python
+  timestamp = datetime.datetime.now().strftime("%Y%m%d")
+  employees[username] = {"emp_id": emp_id,"timestamp":timestamp,"gender": gender,"salary": salary}
+  print("The user",username,"was added")
+
+#display all the employees registered in the system ordered by date
+
+def display_all_employees():
+    sorted_employees = sorted(employees.items(), key=lambda x: x[1]["timestamp"], reverse=True)
+
+    for username, employee in sorted_employees:
+        emp_id = employee["emp_id"]
+        timestamp = datetime.datetime.strptime(employee["timestamp"], "%Y%m%d").strftime("%Y-%m-%d")
+        gender = employee["gender"]
+        salary = employee["salary"]
+    print("Id=",emp_id,",username=",username,",timestamp=",timestamp,",gender=",gender,",salary=",salary)
+
+
+#change the employee's salary
+def change_salary():
+  emp_id=input("Enter the Id: ")
+  salary=int(input("Enter the changed salary: "))
+  if emp_id in employees:
+        employees[emp_id]["salary"] = salary
+        print("Salary changed successfully.")
+    else:
+        print("Id not found.")
+
+# Remove employee from the data
+def remove_employee():
+    emp_id = input("Enter the Id: ")
+
+    if emp_id in employees:
+        del employees[emp_id]
+        print("Employee removed successfully.")
+    else:
+        print("Id not found.")
+  
+# Raise employee's salary
+def raise_salary():
+    emp_id = input("Enter employee Id: ")
+    percentage = float(input("Enter raise percentage: "))
+
+    if emp_id in employees:
+        employees[emp_id]["salary"] *= percentage
+        print("Salary raised successfully.")
+    else:
+        print("Id not found.")
+
+# Save employees to file
+#https://www.w3schools.com/python/ref_file_write.asp
+def save_employees():
+    with open(employees.txt, "w") as file:
+        for username, employee in employees.items():
+            emp_id = employee["emp_id"]
+            timestamp = employee["timestamp"]
+            gender = employee["gender"]
+            salary = employee["salary"]
+            file.write(f"{emp_id},{username},{timestamp},{gender},{salary}\n")
 
 
 
